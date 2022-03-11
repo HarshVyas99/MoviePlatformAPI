7a4e8dc01d166bfcb449ad47ea563a2f3200d139
Harsh <harshvyas9990@gmail.com>
2022-03-12 00:41:14 +0530

    Project Updated files

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
-import django-heroku
+import django_heroku
 
 
 
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STSTIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
