022aea73b0111bb2e1e0f563f44cedb446889346
Harsh <harshvyas9990@gmail.com>
2022-03-12 00:27:50 +0530

    Project Updated

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django-heroku
 
 
 
@@ -156,3 +157,5 @@ REST_FRAMEWORK = {
     # 'PAGE_SIZE': 3
 
 }
+
+django_heroku.settings(locals())
